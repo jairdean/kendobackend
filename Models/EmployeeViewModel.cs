ce35cf35997cbcf6abcf48f56b30d67442749ff7
jairdean <jairdean@hotmail.com>
2024-07-28 00:47:43 -0500

    changeMyTel

--- Models/EmployeeViewModel.cs
+++ Models/EmployeeViewModel.cs
@@ -5,7 +5,7 @@
         public string FullName { get; set; }
         public string EmployeeID { get; set; }
         public decimal SIN { get; set; }
-        public decimal PhoneNumber { get; set; }
+        public string PhoneNumber { get; set; }
         public string City { get; set; }
         public decimal Salary { get; set; }
         public string Country { get; set; }
