8d61ea72bbdce77829ed8f6a28aeb11be7a6732e
msaritama <msaritama@hotmail.com>
2024-07-28 22:39:40 -0500

    Avance Task 2

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -9,70 +9,36 @@ namespace WebAppMVC.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         // GET: EmployeeController
-        public ActionResult Index()
+        public ActionResult Employee()
         {
+            string city = "Mississauga";
+            int count = employees.Count(e => e.City == city);
+            ViewBag.EmployeeCount = count;
             return View();
-        }
-
-        // GET: https://localhost:7245/Employee/LoadEmployees
-        public ViewResult LoadEmployees()
-        {
-            //https://www.youtube.com/watch?v=nHAMDUtiV6w
-            List<EmployeeViewModel> myList = new List<EmployeeViewModel>() {
-
-            new EmployeeViewModel{ FullName = "Miguel Saritama", EmployeeID = "id1", SIN = 11, PhoneNumber = "5612005637", City = "Boynton Beach", Salary = 1111.666m, Country = "USA"},
-            new EmployeeViewModel{ FullName = "Jonathan Valles", EmployeeID = "id2", SIN = 22, PhoneNumber = "2222005637", City = "Delray Beach", Salary = 222.644m, Country = "UNITED STATES"},
-            new EmployeeViewModel{ FullName = "Robert Moran", EmployeeID = "id3", SIN = 33, PhoneNumber = "3332005637", City = "Pompano Beach", Salary = 333.59m, Country = "UNITED STATES OF AMERICA"},
-            new EmployeeViewModel{ FullName = "Jairo Flores", EmployeeID = "id4", SIN = 44, PhoneNumber = "4442005637", City = "Mississauga", Salary = 111, Country = "POLAND"},
-            new EmployeeViewModel{ FullName = "NameF", EmployeeID = "id5", SIN = 55, PhoneNumber = "5552005637", City = "Sarasota", Salary = 111, Country = "GERMANY"},
-            new EmployeeViewModel{ FullName = "NameG", EmployeeID = "id6", SIN = 66, PhoneNumber = "6662005637", City = "Boca Raton", Salary = 111, Country = "UKRAINE"},
-            new EmployeeViewModel{ FullName = "NameH", EmployeeID = "id7", SIN = 77, PhoneNumber = "7772005637", City = "Fort Lauderdale", Salary = 111, Country = "AFRICA"},
-            new EmployeeViewModel{ FullName = "NameI", EmployeeID = "id8", SIN = 88, PhoneNumber = "8882005637", City = "Welington", Salary = 111, Country = "ENGLAND"},
-            new EmployeeViewModel{ FullName = "NameG", EmployeeID = "id9", SIN = 99, PhoneNumber = "9992005637", City = "Congress", Salary = 111, Country = "MEXICO"},
-            new EmployeeViewModel{ FullName = "NameK", EmployeeID = "id10", SIN = 110, PhoneNumber = "5612001010", City = "city kkk", Salary = 111, Country = "HONDURAS"},
-            new EmployeeViewModel{ FullName = "NameL", EmployeeID = "id11", SIN = 111, PhoneNumber = "5612001111", City = "city lll", Salary = 111, Country = "GUATEMALA"},
-            new EmployeeViewModel{ FullName = "NameM", EmployeeID = "id12", SIN = 112, PhoneNumber = "5612001212", City = "Mississauga", Salary = 111, Country = "COSTA RICA"},
-            new EmployeeViewModel{ FullName = "NameN", EmployeeID = "id13", SIN = 113, PhoneNumber = "5612001313", City = "city nnn", Salary = 111, Country = "PUERTO RICO"},
-            new EmployeeViewModel{ FullName = "NameO", EmployeeID = "id14", SIN = 114, PhoneNumber = "5612001414", City = "city ooo", Salary = 111, Country = "CUBA"},
-            new EmployeeViewModel{ FullName = "NameP", EmployeeID = "id15", SIN = 115, PhoneNumber = "5612001515", City = "city ppp", Salary = 111, Country = "PANAMA"},
-            new EmployeeViewModel{ FullName = "NameQ", EmployeeID = "id16", SIN = 116, PhoneNumber = "5612001616", City = "city qqq", Salary = 111, Country = "AUSTRALIA"},
-            new EmployeeViewModel{ FullName = "NameQ", EmployeeID = "id16", SIN = 116, PhoneNumber = "5612001616", City = "city qqq", Salary = 111, Country = "AUSTRALIA"},
-            };
-
-            return View(myList);
-        }
-
-        // GET: https://localhost:7245/Employee/Grid_Read
-        //https://docs.telerik.com/aspnet-core/html-helpers/data-management/grid/getting-started-grid
+        }        
 
         List<EmployeeViewModel> employees = new List<EmployeeViewModel> {
-            new EmployeeViewModel{ FullName = "Miguel Saritama", EmployeeID = "id1", SIN = 11, PhoneNumber = "5612005637", City = "Boynton Beach", Salary = 1111.666m, Country = "USA"},
-            new EmployeeViewModel{ FullName = "Jonathan Valles", EmployeeID = "id2", SIN = 22, PhoneNumber = "2222005637", City = "Delray Beach", Salary = 222.644m, Country = "UNITED STATES"},
-            new EmployeeViewModel{ FullName = "Robert Moran", EmployeeID = "id3", SIN = 33, PhoneNumber = "3332005637", City = "Pompano Beach", Salary = 333.59m, Country = "UNITED STATES OF AMERICA"},
-            new EmployeeViewModel{ FullName = "Jairo Flores", EmployeeID = "id4", SIN = 44, PhoneNumber = "4442005637", City = "Mississauga", Salary = 111, Country = "POLAND"},
-            new EmployeeViewModel{ FullName = "NameF", EmployeeID = "id5", SIN = 55, PhoneNumber = "5552005637", City = "Sarasota", Salary = 111, Country = "GERMANY"},
-            new EmployeeViewModel{ FullName = "NameG", EmployeeID = "id6", SIN = 66, PhoneNumber = "6662005637", City = "Boca Raton", Salary = 111, Country = "UKRAINE"},
-            new EmployeeViewModel{ FullName = "NameH", EmployeeID = "id7", SIN = 77, PhoneNumber = "7772005637", City = "Fort Lauderdale", Salary = 111, Country = "AFRICA"},
-            new EmployeeViewModel{ FullName = "NameI", EmployeeID = "id8", SIN = 88, PhoneNumber = "8882005637", City = "Welington", Salary = 111, Country = "ENGLAND"},
-            new EmployeeViewModel{ FullName = "NameG", EmployeeID = "id9", SIN = 99, PhoneNumber = "9992005637", City = "Congress", Salary = 111, Country = "MEXICO"},
-            new EmployeeViewModel{ FullName = "NameK", EmployeeID = "id10", SIN = 110, PhoneNumber = "5612001010", City = "city kkk", Salary = 111, Country = "HONDURAS"},
-            new EmployeeViewModel{ FullName = "NameL", EmployeeID = "id11", SIN = 111, PhoneNumber = "5612001111", City = "city lll", Salary = 111, Country = "GUATEMALA"},
-            new EmployeeViewModel{ FullName = "NameM", EmployeeID = "id12", SIN = 112, PhoneNumber = "5612001212", City = "Mississauga", Salary = 111, Country = "COSTA RICA"},
-            new EmployeeViewModel{ FullName = "NameN", EmployeeID = "id13", SIN = 113, PhoneNumber = "5612001313", City = "city nnn", Salary = 111, Country = "PUERTO RICO"},
-            new EmployeeViewModel{ FullName = "NameO", EmployeeID = "id14", SIN = 114, PhoneNumber = "5612001414", City = "city ooo", Salary = 111, Country = "CUBA"},
-            new EmployeeViewModel{ FullName = "NameP", EmployeeID = "id15", SIN = 115, PhoneNumber = "5612001515", City = "city ppp", Salary = 111, Country = "PANAMA"},
-            new EmployeeViewModel{ FullName = "NameQ", EmployeeID = "id16", SIN = 116, PhoneNumber = "5612001616", City = "city qqq", Salary = 111, Country = "AUSTRALIA"},
+            new EmployeeViewModel{ FullName = "Miguel Saritama", EmployeeID = "id1", SIN = 111111111, PhoneNumber = "5612005637", City = "Boynton Beach", Salary = 1233.896m, Country = "USA"},
+            new EmployeeViewModel{ FullName = "Jonathan Valles", EmployeeID = "id2", SIN = 222222222, PhoneNumber = "2222005637", City = "Delray Beach", Salary = 222.644m, Country = "UNITED STATES"},
+            new EmployeeViewModel{ FullName = "Robert Moran", EmployeeID = "id3", SIN = 333333333, PhoneNumber = "3332005637", City = "Pompano Beach", Salary = 333.5966m, Country = "UNITED STATES OF AMERICA"},
+            new EmployeeViewModel{ FullName = "Jairo Flores", EmployeeID = "id4", SIN = 4444444, PhoneNumber = "4442005637", City = "Mississauga", Salary = 111, Country = "CANADA"},
+            new EmployeeViewModel{ FullName = "NameF", EmployeeID = "id5", SIN = 555555555, PhoneNumber = "5552005637", City = "Sarasota", Salary = 111, Country = "GERMANY"},
+            new EmployeeViewModel{ FullName = "NameG", EmployeeID = "id6", SIN = 666666666, PhoneNumber = "6662005637", City = "Boca Raton", Salary = 111, Country = "UKRAINE"},
+            new EmployeeViewModel{ FullName = "NameH", EmployeeID = "id7", SIN = 777777777, PhoneNumber = "7772005637", City = "Fort Lauderdale", Salary = 111, Country = "AFRICA"},
+            new EmployeeViewModel{ FullName = "NameI", EmployeeID = "id8", SIN = 888888888, PhoneNumber = "8882005637", City = "Welington", Salary = 111, Country = "ENGLAND"},
+            new EmployeeViewModel{ FullName = "NameG", EmployeeID = "id9", SIN = 999999999, PhoneNumber = "9992005637", City = "Congress", Salary = 111, Country = "MEXICO"},
+            new EmployeeViewModel{ FullName = "NameK", EmployeeID = "id10", SIN = 110111111, PhoneNumber = "5612001010", City = "city kkk", Salary = 111, Country = "HONDURAS"},
+            new EmployeeViewModel{ FullName = "NameL", EmployeeID = "id11", SIN = 111111111, PhoneNumber = "5612001111", City = "city lll", Salary = 111, Country = "GUATEMALA"},
+            new EmployeeViewModel{ FullName = "NameM", EmployeeID = "id12", SIN = 112111111, PhoneNumber = "5612001212", City = "Mississauga", Salary = 111, Country = "COSTA RICA"},
+            new EmployeeViewModel{ FullName = "NameN", EmployeeID = "id13", SIN = 113111111, PhoneNumber = "5612001313", City = "city nnn", Salary = 111, Country = "PUERTO RICO"},
+            new EmployeeViewModel{ FullName = "NameO", EmployeeID = "id14", SIN = 114111111, PhoneNumber = "5612001414", City = "city ooo", Salary = 111, Country = "CUBA"},
+            new EmployeeViewModel{ FullName = "NameP", EmployeeID = "id15", SIN = 115111111, PhoneNumber = "5612001515", City = "city ppp", Salary = 111, Country = "CANADA"},
+            new EmployeeViewModel{ FullName = "NameQ", EmployeeID = "id16", SIN = 116111111, PhoneNumber = "5612001616", City = "city qqq", Salary = 111, Country = "AUSTRALIA"},
         };
 
         public JsonResult Grid_Read([DataSourceRequest] DataSourceRequest request)
         {
             return Json(employees.ToDataSourceResult(request));
-        }
-
-        public JsonResult GetEmployeeCountByCity(string city)
-        {
-            int count = employees.Count(e => e.City == city);
-            return Json(new { City = city, Count = count });
         }
 
         // GET: EmployeeController/Details/5
--- Program.cs
+++ Program.cs
@@ -30,6 +30,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Employee}/{action=Employee}");
 
 app.Run();
